9d6bfcfb64dbd87e4e20ea04a390f8d69614d92a
Daigo Terazono <terazono.daigo.p4@dc.tohoku.ac.jp>
2024-05-01 18:16:20 +0900

    .gitignoreに新しいファイルとディレクトリを追加しました settings.pyにSECRET_KEYの生成とALLOWED_HOSTSの更新を行いました

--- powerJudgeproject/settings.py
+++ powerJudgeproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = get_random_secret_key()  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r$@le5wx+0lu^ozlom(e)yp+n%)8ndl=a@g3gyts3@)8w0+e^o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'daigo.pythonanywhere.com']
 
 
 # Application definition
@@ -128,4 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
+try:
+    from .local_settings import *
+except:
+    pass
